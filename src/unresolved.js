51736735eec6d8e9ffa4f67f97189d1c78930401
Steve Orvell <steve@orvellia.com>
2014-01-30 10:01:14 -0800

    simplify styling.

--- src/unresolved.js
+++ src/unresolved.js
@@ -11,18 +11,11 @@
   // It would make sense to put inside some HTMLImport but 
   // the HTMLImports polyfill does not allow loading of stylesheets that blocks
   // rendering. Therefore this is being tolerated here.
-  var TRANSITION_TIME = 0.2;
   var UNRESOLVED = 'unresolved';
-  var RESOLVED = 'resolved';
   var UNRESOLVED_SELECTOR = '[' + UNRESOLVED + ']';
-  var RESOLVED_SELECTOR = '[' + RESOLVED + ']';
   var style = document.createElement('style');
-  
   style.textContent = UNRESOLVED_SELECTOR + ' { ' +
       'opacity: 0; display: block; overflow: hidden; } \n' +
-      RESOLVED_SELECTOR +  '{ display: block; overflow: hidden;\n' +
-      '-webkit-transition: opacity ' + TRANSITION_TIME + 's; ' +
-      'transition: opacity ' + TRANSITION_TIME +'s; }\n';
   var head = document.querySelector('head');
   head.insertBefore(style, head.firstChild);
 
