25fad6d6dd91a83b7f1b65328c19494202e6f097
Steve Orvell <steve@orvellia.com>
2014-01-30 09:57:09 -0800

    Fixes #5

--- src/unresolved.js
+++ src/unresolved.js
@@ -4,13 +4,13 @@
  * license that can be found in the LICENSE file.
  */
 (function(scope) {
-  /*
-    Shim :unresolved via an attribute unresolved.
-    TODO(sorvell): This is currently done once when the first
-    round of elements are upgraded. This is incorrect, and it will
-    need to be done dynamically.
-    The 'resolved' attribute is experimental and may be removed.
-  */
+  
+  // TODO(sorvell): It's desireable to provide a default stylesheet 
+  // that's convenient for styling unresolved elements, but
+  // it's cumbersome to have to include this manually in every page.
+  // It would make sense to put inside some HTMLImport but 
+  // the HTMLImports polyfill does not allow loading of stylesheets that blocks
+  // rendering. Therefore this is being tolerated here.
   var TRANSITION_TIME = 0.2;
   var UNRESOLVED = 'unresolved';
   var RESOLVED = 'resolved';
@@ -25,35 +25,5 @@
       'transition: opacity ' + TRANSITION_TIME +'s; }\n';
   var head = document.querySelector('head');
   head.insertBefore(style, head.firstChild);
-
-  // remove unresolved and apply resolved class
-  function resolveElements() {
-    requestAnimationFrame(function() {
-      var nodes = document.querySelectorAll(UNRESOLVED_SELECTOR);
-      for (var i=0, l=nodes.length, n; (i<l) && (n=nodes[i]); i++) {
-        n.removeAttribute(UNRESOLVED);
-        n.setAttribute(RESOLVED, '');
-      }
-
-      // NOTE: depends on transition end event to remove 'resolved' class.
-      if (nodes.length) {
-        var removeResolved = function() {
-          for (var i=0, l=nodes.length, n; (i<l) && (n=nodes[i]); i++) {
-            n.removeAttribute(RESOLVED);
-          }
-          document.body.removeEventListener(endEvent, removeResolved, false);
-        }
-        document.body.addEventListener(endEvent, removeResolved, false);
-      };
-
-    });
-  }
-
-  // determine transition end event
-  var endEvent = (document.documentElement.style.webkitTransition !== undefined) ?
-      'webkitTransitionEnd' : 'transitionend';
-
-  // hookup auto-unveiling
-  window.addEventListener('WebComponentsReady', resolveElements);
 
 })(Platform);
