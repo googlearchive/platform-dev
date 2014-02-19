09d2e1fe7892a349f42d0ec32e81f2d2552f6ab0
Steve Orvell <steve@orvellia.com>
2014-02-19 10:23:14 -0800

    remove platform caching of css in favor of xhr'ing. The caching was too brittle as it relied on browser cache state.

--- src/styleloader.js
+++ src/styleloader.js
@@ -44,59 +44,25 @@ StyleResolver.prototype = {
       text = text.replace(match.matched, intermediate);
     }
     return text;
-  }
-};
-
-var styleResolver = new StyleResolver();
-var loader = {
-  cacheStyles: function(styles, callback) {
-    var css = [];
-    for (var i=0, l=styles.length, s; (i<l) && (s=styles[i]); i++) {
-      css.push(s.textContent);
-    }
-    cacheCssText(css.join('\n'), callback);
   },
-  xhrStyles: function(styles, callback) {
+  loadStyles: function(styles, callback) {
     var loaded=0, l = styles.length;
     // called in the context of the style
     function loadedStyle(style) {
-      //console.log(style.textContent);
       loaded++;
       if (loaded === l && callback) {
         callback();
       }
     }
     for (var i=0, s; (i<l) && (s=styles[i]); i++) {
-      styleResolver.resolveNode(s, loadedStyle);
+      this.resolveNode(s, loadedStyle);
     }
   }
 };
 
-// use the platform to preload styles
-var preloadElement = document.createElement('preloader');
-preloadElement.style.display = 'none';
-var preloadRoot = preloadElement.createShadowRoot();
-document.head.appendChild(preloadElement);
-
-function cacheCssText(cssText, callback) {
-  var style = createStyleElement(cssText);
-  if (callback) {
-    style.addEventListener('load', callback);
-    style.addEventListener('error', callback);
-  }
-  preloadRoot.appendChild(style);
-}
-
-function createStyleElement(cssText, scope) {
-  scope = scope || document;
-  scope = scope.createElement ? scope : scope.ownerDocument;
-  var style = scope.createElement('style');
-  style.textContent = cssText;
-  return style;
-}
+var styleResolver = new StyleResolver();
 
 // exports
-scope.loader = loader;
 scope.styleResolver = styleResolver;
 
 })(window.Platform);
